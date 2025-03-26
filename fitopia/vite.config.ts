14688739a997281485eed0b17a4390bf2d99936c
Jonathan Zhang <yufengzhang483@gmail.com>
2025-03-26 18:15:52 -0400

    Update base path for GitHub Pages

--- fitopia/vite.config.ts
+++ fitopia/vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/feurther.com/", // 替换为你的仓库名
+  base: "/feurther-technologies-llc.github.io/",
 });
