1a2c053c4915c8223bcc2f5b7033ab441558bf3d
Jonathan Zhang <yufengzhang483@gmail.com>
2025-05-09 23:36:56 -0400

    update

--- fitopia/vite.config.ts
+++ fitopia/vite.config.ts
@@ -5,6 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://feurther.com/",
+  base: "/feurther.com/",
   assetsInclude: ["**/*.PNG"],
 });
